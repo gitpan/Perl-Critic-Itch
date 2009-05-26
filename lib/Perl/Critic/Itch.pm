ed35cd63fc8965f1ff4d0605ffa5c43fb540f45e
Marcelo Rodrigues <marcelo@cpan.org>
2009-05-26 14:41:57 +0000

    Import of MARCELO/Perl-Critic-Itch-0.02 from CPAN.
    gitpan-cpan-distribution: Perl-Critic-Itch
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MARCELO/Perl-Critic-Itch-0.02.tar.gz
    gitpan-cpan-author:       MARCELO
    gitpan-cpan-maturity:     released

--- lib/Perl/Critic/Itch.pm
+++ lib/Perl/Critic/Itch.pm
@@ -9,11 +9,11 @@ Perl::Critic::Itch - A collection of Policies to solve some Itches
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
