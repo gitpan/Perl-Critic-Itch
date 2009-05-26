ab3c46a95b9bd5dcdb8a44c313bb1b5d5f27e4ac
Marcelo Rodrigues <marcelo@cpan.org>
2009-05-26 13:33:59 +0000

    Import of MARCELO/Perl-Critic-Itch-0.01 from CPAN.
    gitpan-cpan-distribution: Perl-Critic-Itch
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MARCELO/Perl-Critic-Itch-0.01.tar.gz
    gitpan-cpan-author:       MARCELO
    gitpan-cpan-maturity:     released

--- lib/Perl/Critic/Itch.pm
+++ lib/Perl/Critic/Itch.pm
@@ -5,38 +5,41 @@ use strict;
 
 =head1 NAME
 
-Perl::Critic::Itch - A collection of Perl::Critic policies that causes me Itch
+Perl::Critic::Itch - A collection of Policies to solve some Itches
 
 =head1 VERSION
 
-Version 1.0
+Version 0.01
 
 =cut
 
-our $VERSION = '1.0';
+our $VERSION = '0.01';
+
 
 =head1 SYNOPSIS
 
-Perl::Critic::Itch - A remedy for some stuff that causes me Itches
-
-=head1 DESCRIPTION
-
-For now, there's only one rule included in Perl::Critic::Itch:
-
+## Please see file perltidy.ERR
+Perl::Critic::Itch was created to furfill some special requests when 
+analysing Perl Code. This policies, may not be usefull to everyone,
+and surely not consensual, but the solve some little itches i had,
+and makes me sleep better at night !
+  
+List of Policies implemented:
 =head2 L<Perl::Critic::Policy::CodeLayout::ProhibitHashBarewords>
-
-Commented-out code is usually noise.  It should be removed.
 
 
 =head1 AUTHOR
 
-Marcelo, C<< <marcelo at cpan.org> >>
+Marcelo Rodrigues, C<< <marcelo at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to
-C<bug-perl-critic-itch at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Perl-Critic-Itchs>.
+Please report any bugs or feature requests to C<bug-perl-critic-itch at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Perl-Critic-Itch>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
 
 =head1 SUPPORT
 
@@ -44,16 +47,39 @@ You can find documentation for this module with the perldoc command.
 
     perldoc Perl::Critic::Itch
 
-=head1 COPYRIGHT
 
-This library is free software; you can redistribute it and/or modify
-it under the terms of either the GNU Public License v3, or the Artistic
-License 2.0.
+You can also look for information at:
 
-    * http://www.gnu.org/copyleft/gpl.html
+=over 4
 
-    * http://www.opensource.org/licenses/artistic-license-2.0.php
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Perl-Critic-Itch>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Perl-Critic-Itch>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Perl-Critic-Itch>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Perl-Critic-Itch/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
 
 =cut
 
-1; 
+1; # End of Perl::Critic::Itch
--- t/00-load.t
+++ t/00-load.t
@@ -1,15 +1,9 @@
-#!/usr/bin/perl
+#!perl -T
 
 use Test::More tests => 1;
 
-diag(
-  "Perl::Critic::Ithc $Perl::Critic::Policy::Itch::VERSION, Perl $],$^X"
-);
-
 BEGIN {
-  use_ok(
-    'Perl::Critic::Policy::CodeLayout::ProhibitHashBarewords'
-  );
+	use_ok( 'Perl::Critic::Itch' );
 }
 
-
+diag( "Testing Perl::Critic::Itch $Perl::Critic::Itch::VERSION, Perl $], $^X" );
